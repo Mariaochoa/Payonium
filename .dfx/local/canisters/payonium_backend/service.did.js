e141e01b92f4b313c6b83ce4d4b7bb7d7874623e
GFuentesG <g7fuentes7@gmail.com>
2025-03-06 12:40:09 -0500

    Eliminar node_modules y .dfx/local del repositorio

--- .dfx/local/canisters/payonium_backend/service.did.js
+++
@@ -1,7 +0,0 @@
-export const idlFactory = ({ IDL }) => {
-  return IDL.Service({
-    'greet' : IDL.Func([IDL.Text], [IDL.Text], ['query']),
-    'whoAmI' : IDL.Func([], [IDL.Principal], ['query']),
-  });
-};
-export const init = ({ IDL }) => { return []; };
