ebd508fdbeb2e3c75225177993ddc9a97b8f0631
GFuentesG <g7fuentes7@gmail.com>
2025-03-10 18:49:46 -0500

    conectando funcion ver registros del front con el back

--- src/payonium_frontend/src/App.jsx
+++ src/payonium_frontend/src/App.jsx
@@ -20,6 +20,7 @@ function App() {
 
   const [greeting, setGreeting] = useState('');
   const [principal, setPrincipal] = useState('');
+  const [profiles, setProfiles] = useState([]);
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -41,6 +42,22 @@ function App() {
     setPrincipal(principal.toString());
   }
 
+  async function getProfiles() {
+
+    try {
+      const result = await backend.getProfiles();
+      console.log(result);
+      if (result.ok.profiles){
+        setProfiles(result.ok.profiles);
+      } else {
+        alert("Error al obtener los perfiles");
+      } 
+      
+    }  catch (err) {
+        console.log(err);
+    };
+  };
+
   return (
     <div >
       <header className="App-header">
@@ -49,7 +66,7 @@ function App() {
       </header>
     <main>
       <img src="/payonium.jpg" alt="PAYONIUM logo" />
-      <br />
+
       <br />
       <form action="#" onSubmit={handleSubmit}>
         <label htmlFor="name">Enter your name: &nbsp;</label>
@@ -63,6 +80,31 @@ function App() {
       <button onClick={handleWhoAmI}>Who Am I?</button>
       </div>
       <section id="principal">{principal}</section>
+
+      <br />
+        <div>
+          <button onClick={getProfiles}>Get User Profiles</button>
+        </div>
+        <section id="profiles">
+         
+            <ul>
+              {profiles.map((profile, index) => (
+                <li key={index}>
+                  <strong>{profile.name} {profile.lastname}</strong><br />
+                  <span>Email: {profile.email}</span><br />
+                  <span>DNI: {profile.dni}</span><br />
+                  <span>Country of Origin: {profile.countryorigindocument}</span>
+                  <span>Phone: {profile.phone}</span><br />
+                  <span>Password: {profile.password}</span><br />
+                  <span>Country of Residence: {profile.countryresidence}</span><br />
+                  <span>Owner: {JSON.stringify(profile.owner)}</span><br />
+                  <span>Role: {Object.keys(profile.role).find(key => profile.role[key] === null) }</span><br />
+
+                </li>
+              ))}
+            </ul>
+        
+        </section>
 
     </main>
     </div>
