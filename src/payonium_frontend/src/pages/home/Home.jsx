100508d596b466425e4c6858f942744f1284ba10
GFuentesG <g7fuentes7@gmail.com>
2025-03-18 11:49:47 -0500

    agregando paginas home / profile, estilos como modulos

--- src/payonium_frontend/src/pages/home/Home.jsx
+++ src/payonium_frontend/src/pages/home/Home.jsx
@@ -1,180 +1,75 @@
-import { useContext, useState } from 'react';
-import LoginButton from '../../components/auth/LoginButton';
-import LogoutButton from '../../components/auth/LogoutButton';
-import { AuthContext } from '../../context/AuthContext';
-import { createActor } from 'declarations/payonium_backend';
-import '../../App.css';
+import React from "react";
+import styles from "./Home.module.css"; // Asegúrate de tener los estilos correspondientes
 
-function Home() {
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Bienvenido a Payonium</h1>
+      <p className={styles.subtitle}>Transformando los pagos transfronterizos para tu negocio</p>
 
-      const { isAuthenticated, identity } = useContext(AuthContext);
-      
-      let canisterId = process.env.CANISTER_ID_PAYONIUM_BACKEND;
-    
-      let backend = createActor(canisterId, {
-        agentOptions: {
-          identity: identity,
-          host: "http://localhost:4943",
-        },
-      });
-    
-      const [greeting, setGreeting] = useState('');
-      const [principal, setPrincipal] = useState('');
-      const [profiles, setProfiles] = useState([]);
-    
-      const [name, setName] = useState('');
-      const [lastname, setLastname] = useState('');
-      const [email, setEmail] = useState('');
-      const [dni, setDni] = useState('');
-      const [countryorigindocument, setCountryorigindocument] = useState('');
-      const [phone, setPhone] = useState('');
-      const [password, setPassword] = useState('');
-      const [countryresidence, setCountryresidence] = useState('');
-    
-      function handleSubmit(event) {
-        event.preventDefault();
-        const name = event.target.elements.name.value;
-    
-        if(!isAuthenticated){
-          alert("Por favor, usted debe loguearse primero para ejecutar esta funcion");
-          return;
-        }
-    
-        backend.greet(name).then((greeting) => {
-          setGreeting(greeting);
-        });
-        return false;
-      }
-    
-      async function handleWhoAmI() {
-        const principal = await backend.whoAmI();
-        setPrincipal(principal.toString());
-      }
-    
-      async function getProfiles() {
-    
-        try {
-          const result = await backend.getProfiles();
-          console.log(result);
-          if (result.ok.profiles){
-            setProfiles(result.ok.profiles);
-          } else {
-            alert("Error al obtener los perfiles");
-          } 
-          
-        }  catch (err) {
-            console.log(err);
-        };
-      };
-    
-      async function registerUser(event) {
-        event.preventDefault();
-        if (!isAuthenticated) {
-          alert("Debe estar logueado para registrar un usuario.");
-          return;
-        }
-    
-        const newProfile = {
-          name, lastname, email, dni, countryorigindocument, phone, password, countryresidence, 
-          owner: identity.getPrincipal(), 
-          role: 'user',
-        };
-        try {
-          console.log(newProfile);
-          const result = await backend.registerUser(newProfile);
-          if(result) {
-            alert("usuario registrado exitosamente");
-    
-          }else {
-            alert("error en el registro: " + result.err)
-          };
-        } catch (err) {
-          console.log(err)
-        }
-        
-      }
-    
-    
-
-   return (
-      <div >
-        <header className="App-header">
-          {isAuthenticated ? <LogoutButton /> : <LoginButton />}
-          
-        </header>
-      <main>
-        <img src="/logopy.png" alt="PAYONIUM logo" className="logopayonium"/>
-  
-        <br />
-        <form action="#" onSubmit={handleSubmit}>
-          <label htmlFor="name">Enter your name: &nbsp;</label>
-          <input id="name" alt="Name" type="text" />
-          <button type="submit">Click Me!</button>
-        </form>
-        <section id="greeting">{greeting}</section>
-  
-        <br />
-        <div>
-        <button onClick={handleWhoAmI}>Who Am I?</button>
+      <section className={styles.section}>
+        <div className={styles.imageContainer}>
+          <img
+            src="/payment-processing.jpg"
+            alt="Procesamiento de pagos"
+            className={styles.image}
+          />
         </div>
-        <section id="principal">{principal}</section>
-  
-        <br />
-          <div>
-            <button onClick={getProfiles}>Get User Profiles</button>
-          </div>
-          <section id="profiles">
-           
-              <ul>
-                {profiles.map((profile, index) => (
-                  <li key={index}>
-                    <strong>{profile.name} {profile.lastname}</strong><br />
-                    <span>Email: {profile.email}</span><br />
-                    <span>DNI: {profile.dni}</span><br />
-                    <span>Country of Origin: {profile.countryorigindocument}</span>
-                    <span>Phone: {profile.phone}</span><br />
-                    <span>Password: {profile.password}</span><br />
-                    <span>Country of Residence: {profile.countryresidence}</span><br />
-                    <span>Owner: {JSON.stringify(profile.owner)}</span><br />
-                    <span>Role: {profile.role}</span><br />
-                  </li>
-                ))}
-              </ul>
-          
-          </section>
-  
-          <label>Registro de Usuario</label>
-          <br />
-          <form onSubmit={registerUser}>
-            <input
-              type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} required
-            />
-            <input
-              type="text" placeholder="Lastname" value={lastname} onChange={(e) => setLastname(e.target.value)} required
-            />
-            <input
-              type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required
-            />
-            <input
-              type="text" placeholder="DNI" value={dni} onChange={(e) => setDni(e.target.value)} required
-            />
-            <input
-              type="text" placeholder="Country origin ID" value={countryorigindocument} onChange={(e) => setCountryorigindocument(e.target.value)}  required
-            />
-            <input
-              type="text" placeholder="Phone" value={phone} onChange={(e) => setPhone(e.target.value)} required
-            />
-            <input
-              type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required
-            />
-            <input
-              type="text" placeholder="Country residence" value={countryresidence} onChange={(e) => setCountryresidence(e.target.value)} required
-            />
-            <button type="submit">Registrar Usuario</button>
-          </form>
-      </main>
-      </div>
-    );
-}
+        <div className={styles.textContainer}>
+          <h2 className={styles.sectionTitle}>Procesador de Pagos Global</h2>
+          <p>
+            Payonium es una plataforma de pagos que te permite aceptar pagos
+            en cualquier moneda y por diversos métodos, sin importar la ubicación
+            de tus clientes. Desde tarjetas de crédito hasta métodos de pago
+            locales, Payonium facilita las transacciones de manera sencilla y
+            rápida, permitiendo a los negocios recibir pagos de manera
+            eficiente y segura.
+          </p>
+        </div>
+      </section>
 
-export default Home
+      <section className={styles.section}>
+        <div className={styles.textContainer}>
+          <h2 className={styles.sectionTitle}>Soluciones Flexibles para Negocios</h2>
+          <p>
+            Payonium ofrece soluciones adaptables para que tu negocio pueda
+            gestionar pagos de manera eficiente. Con Payonium, puedes crear
+            enlaces de pago personalizados y facilitar la integración en tu
+            tienda en línea. Ya sea que vendes productos físicos, servicios o
+            contenido digital, nuestra plataforma te ayuda a gestionar todos
+            los pagos de manera centralizada.
+          </p>
+        </div>
+        <div className={styles.imageContainer}>
+          <img
+            src="/payment-solutions.jpg"
+            alt="Soluciones de pago"
+            className={styles.image}
+          />
+        </div>
+      </section>
+
+      <section className={styles.section}>
+        <div className={styles.imageContainer}>
+          <img
+            src="/payment-options.png"
+            alt="Opciones de pago"
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.textContainer}>
+          <h2 className={styles.sectionTitle}>Opciones de Pago Internacionales</h2>
+          <p>
+            Con Payonium, puedes ofrecer a tus clientes la flexibilidad de pagar
+            en su moneda local. Ya sea con tarjeta de crédito, transferencias
+            bancarias o métodos de pago populares en su región, nuestra plataforma
+            elimina las barreras y facilita las transacciones internacionales
+            sin complicaciones, lo que te permite expandir tu negocio globalmente.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
--- src/payonium_frontend/src/pages/profile/Profile.jsx
+++ src/payonium_frontend/src/pages/profile/Profile.jsx
@@ -1,7 +1,140 @@
-import React from 'react'
+import { useContext, useState } from 'react';
 
-export default function Profile() {
+import { AuthContext } from '../../context/AuthContext';
+import { createActor } from 'declarations/payonium_backend';
+import styles from './Profile.module.css';
+
+function Profile() {
+
+  const { isAuthenticated, identity } = useContext(AuthContext);
+  
+  let canisterId = process.env.CANISTER_ID_PAYONIUM_BACKEND;
+
+  let backend = createActor(canisterId, {
+    agentOptions: {
+      identity: identity,
+      host: "http://localhost:4943",
+    },
+  });
+
+
+  const [principal, setPrincipal] = useState('');
+  const [profiles, setProfiles] = useState([]);
+
+  const [name, setName] = useState('');
+  const [lastname, setLastname] = useState('');
+  const [email, setEmail] = useState('');
+  const [dni, setDni] = useState('');
+  const [countryorigindocument, setCountryorigindocument] = useState('');
+  const [phone, setPhone] = useState('');
+  const [password, setPassword] = useState('');
+  const [countryresidence, setCountryresidence] = useState('');
+
+ 
+  async function handleWhoAmI() {
+    const principal = await backend.whoAmI();
+    setPrincipal(principal.toString());
+  }
+
+  async function getProfiles() {
+
+    try {
+      const result = await backend.getProfiles();
+      console.log(result);
+      if (result.ok.profiles){
+        setProfiles(result.ok.profiles);
+      } else {
+        alert("Error al obtener los perfiles");
+      } 
+      
+    }  catch (err) {
+        console.log(err);
+    };
+  };
+
+  async function registerUser(event) {
+    event.preventDefault();
+    if (!isAuthenticated) {
+      alert("Debe estar logueado para registrar un usuario.");
+      return;
+    }
+
+    const newProfile = {
+      name, lastname, email, dni, countryorigindocument, phone, password, countryresidence, 
+      owner: identity.getPrincipal(), 
+      role: 'user',
+    };
+    try {
+      console.log(newProfile);
+      const result = await backend.registerUser(newProfile);
+      if(result) {
+        alert("usuario registrado exitosamente");
+
+      }else {
+        alert("error en el registro: " + result.err)
+      };
+    } catch (err) {
+      console.log(err)
+    }
+    
+  }
+
+
+
+
   return (
-    <div>Profile</div>
-  )
+    <div className={styles.container}>
+     
+    
+      <div className={styles.profileSection}>
+        <button onClick={handleWhoAmI}>Who Am I?</button>
+        <section id="principal">{principal}</section>
+      </div>
+      
+
+        <div>
+          <button onClick={getProfiles}>Get User Profiles</button>
+        </div>
+        <section id="profiles">
+         
+            <ul>
+              {profiles.map((profile, index) => (
+                <li key={index}>
+                  <strong>{profile.name} {profile.lastname}</strong><br />
+                  <span>Email: {profile.email}</span><br />
+                  <span>DNI: {profile.dni}</span><br />
+                  <span>Country of Origin: {profile.countryorigindocument}</span>
+                  <span>Phone: {profile.phone}</span><br />
+                  <span>Password: {profile.password}</span><br />
+                  <span>Country of Residence: {profile.countryresidence}</span><br />
+                  <span>Owner: {JSON.stringify(profile.owner)}</span><br />
+                  <span>Role: {profile.role}</span><br />
+                </li>
+              ))}
+            </ul>
+        
+        </section>
+
+        <label>User registration</label>
+        <br />
+        <form onSubmit={registerUser}>
+          <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} required />
+          <input type="text" placeholder="Lastname" value={lastname} onChange={(e) => setLastname(e.target.value)} required />
+          <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+          <input type="text" placeholder="DNI" value={dni} onChange={(e) => setDni(e.target.value)} required />
+          <input type="text" placeholder="Country origin ID" value={countryorigindocument} onChange={(e) => setCountryorigindocument(e.target.value)}  required />
+          <input type="text" placeholder="Phone" value={phone} onChange={(e) => setPhone(e.target.value)} required />
+          <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+          <input type="text" placeholder="Country residence" value={countryresidence} onChange={(e) => setCountryresidence(e.target.value)} required />
+          
+          <div className={styles.formFooter} >
+            <button type="submit">Confirm</button>
+          </div>
+          
+        </form>
+
+    </div>
+  );
 }
+
+export default Profile;
